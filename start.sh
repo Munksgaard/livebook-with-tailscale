051a108e1739c72396f7625ace74e8cd861cc53c
Philip Munksgaard <philip@munksgaard.me>
2024-08-09 13:26:14 +0200

    fix: listen on 0.0.0.0

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 /app/bin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/bin/tailscale up --authkey=${TAILSCALE_AUTHKEY}
 
-export LIVEBOOK_IP=$(/app/bin/tailscale ip -1 | tr -d '\n')
+export LIVEBOOK_IP=0.0.0.0
 
 /app/bin/server
